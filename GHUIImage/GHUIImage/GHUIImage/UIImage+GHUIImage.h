0e6a491a17fb530661e366fec10b074b33629954
joinup <shen_sgh@icloud.com>
2016-02-29 11:47:15 +0800

    finished 带文字的图片

--- GHUIImage/GHUIImage/GHUIImage/UIImage+GHUIImage.h
+++ GHUIImage/GHUIImage/GHUIImage/UIImage+GHUIImage.h
@@ -10,4 +10,25 @@
 
 @interface UIImage (GHUIImage)
 
+/**
+ *  取颜色color背景图片
+ *
+ *  @param color color
+ *  @param frame frame
+ *
+ *  @return UIImage
+ */
++ (UIImage *)imageFormColor:(UIColor *)color frame:(CGRect)frame;
+
+
+/**
+ *  带居中文字的图片
+ *
+ *  @param title    文字
+ *  @param fontSize 文字大小
+ *
+ *  @return UIImage
+ */
+- (UIImage *)imageWithTitle:(NSString *)title fontSize:(CGFloat)fontSize;
+
 @end
--- GHUIImage/GHUIImage/ViewController.m
+++ GHUIImage/GHUIImage/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "UIImage+GHUIImage.h"
 
 @interface ViewController ()
 
+@property (nonatomic,strong) UIImageView *imageView;
 @end
 
 @implementation ViewController
@@ -17,6 +19,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self.view addSubview:self.imageView];
+}
+
+- (UIImageView *)imageView{
+    if (!_imageView) {
+        _imageView=[[UIImageView alloc]init];
+        [_imageView setCenter:self.view.center];
+        [_imageView setBounds:CGRectMake(0.0, 0.0, 50.0, 50.0)];
+        [_imageView.layer setCornerRadius:25.0];
+        [_imageView.layer setMasksToBounds:YES];
+        [_imageView setBackgroundColor:[UIColor colorWithRed:254.0/255 green:52.0/255 blue:134.0/255 alpha:1.0]];
+        [_imageView setImage:[[UIImage imageFormColor:[UIColor colorWithRed:254.0/255 green:52.0/255 blue:134.0/255 alpha:1.0] frame:_imageView.bounds] imageWithTitle:@"李芳" fontSize:14.0]];
+    }
+    return _imageView;
 }
 
 - (void)didReceiveMemoryWarning {
