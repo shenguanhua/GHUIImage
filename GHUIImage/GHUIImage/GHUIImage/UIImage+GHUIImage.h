8f4a7b96e61ad0682813efa71ea9cae47b2af843
joinup <shen_sgh@icloud.com>
2016-02-29 11:29:04 +0800

    create GHUIImage files

---
+++ GHUIImage/GHUIImage/GHUIImage/UIImage+GHUIImage.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+GHUIImage.h
+//  GHUIImage
+//
+//  Created by shen_gh on 16/2/29.
+//  Copyright © 2016年 com.joinup(Beijing). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (GHUIImage)
+
+@end
---
+++ GHUIImage/GHUIImage/GHUIImage/UIImage+GHUIImage.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+GHUIImage.m
+//  GHUIImage
+//
+//  Created by shen_gh on 16/2/29.
+//  Copyright © 2016年 com.joinup(Beijing). All rights reserved.
+//
+
+#import "UIImage+GHUIImage.h"
+
+@implementation UIImage (GHUIImage)
+
+@end
