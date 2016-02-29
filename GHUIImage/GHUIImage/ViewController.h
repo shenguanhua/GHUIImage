a2539cf3e60571b7b72913ce25d885d23d78bac4
joinup <shen_sgh@icloud.com>
2016-02-29 11:13:30 +0800

    Create GHUIImage origin

---
+++ GHUIImage/GHUIImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHUIImage
+//
+//  Created by shen_gh on 16/2/29.
+//  Copyright © 2016年 com.joinup(Beijing). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GHUIImage/GHUIImage/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GHUIImage
+//
+//  Created by shen_gh on 16/2/29.
+//  Copyright © 2016年 com.joinup(Beijing). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
